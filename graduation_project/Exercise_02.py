f4e91804a10017e531e6e5ae4fff50719b210beb
Sergey Karakulanov <54935751+SergeyKa-cmd@users.noreply.github.com>
2020-10-08 17:02:20 +0300

    Delete Exercise_02.py

--- graduation_project/Exercise_02.py
+++
@@ -1,8 +0,0 @@
-import sklearn.datasets
-import load_digits
-digits = load_digits()
-print(digits.data.shape)
-import matplotlib.pyplot as pyplot
-plt.gray()
-plt.matshow(digits.images[2])
-plt.show()
